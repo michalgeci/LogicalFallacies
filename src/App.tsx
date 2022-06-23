a732c8525fffde64448b1c318663d39f147a05cb
Michal Geci <michal.geci.1995@gmail.com>
2022-06-23 12:40:01 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd-mobile'
 import './App.css';
+import fallacies from './fallacies.json';
 
 function App() {
+
+  const [installModalVisible, setInstallModalVisible] = useState(false)
+  const [installEvent, setInstallEvent] = useState<Event>()
+
+  window.addEventListener('appinstalled', (evt) => {
+    setInstallModalVisible(false)
+  })
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    setInstallEvent(e)
+    setInstallModalVisible(true)
+  })
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * fallacies.length)
+  }
+
+  const [randomNumber, setRandomNumber] = useState(getRandomNumber())
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 style={{fontFamily: 'CinzelDecorative'}}>{fallacies[randomNumber].title}</h1>
+
+      <p style={{fontFamily: 'Battambang'}}>{fallacies[randomNumber].description}</p>
+
+      <small style={{fontFamily: 'Battambang'}}>{fallacies[randomNumber].alt}</small> <br/>
+
+      <Button style={{marginTop: 48}} color='default' fill='outline' onClick={() => {
+        window.open('https://www.logicallyfallacious.com' + fallacies[randomNumber].url);
+      }}>
+        Go to detail
+      </Button>
+
+      <br/>
+
+      <Button style={{marginTop: 8}} color='primary' fill='none' onClick={(e) => {
+        setRandomNumber(getRandomNumber())
+      }}>
+        Show random
+      </Button>
+
+      <Modal 
+        visible={installModalVisible}
+        title={'Install PWA application'}
+        content={<p>Please install app for better user experience.</p>}
+        onAction={()=>{}}
+        actions={[
+          {
+            key: 'install', 
+            text: 'INSTALL',
+            primary: true,
+            onClick: () => {
+              (installEvent as any)?.prompt()
+              setInstallModalVisible(false)
+            }
+          }, {
+            key: 'no', 
+            text: 'NO',
+            onClick: () => {
+              setInstallModalVisible(false)
+            }
+          }
+        ]}
+      />
     </div>
   );
 }
